f5a34bb2e86aa2dcba04fe8b84175640d15eb342
d0mk <dominik.kobyra@gmail.com>
2020-11-06 13:00:19 +0100

    upload

--- task.py
+++ task.py
@@ -1,34 +1,28 @@
-#Matrix. 
+class Matrix2x2:
+    def __init__(self, x11, x12, x21, x22):
+        self.data = [x11, x12, x21, x22]
+
+    def dot_product(self, m2):
+        pass
+
+    def __add__(self, m2):
+        if isinstance(m2, Matrix2x2):
+            return Matrix2x2(*[self.data[i] + m2.data[i] for i in range(4)])
+        elif isinstance(m2, int) or isinstance(m2, float):
+            return Matrix2x2(*[self.data[i] + m2 for i in range(4)])
+        else:
+            print('Argument must be of type Matrix2x2')
+
+    def __str__(self):
+        return f'{self.data[0]} {self.data[1]}\n{self.data[2]} {self.data[3]}'
 
 
-#Write a class that can represent any 4𝑥4 real matrix. 
-#Include two functions to calculate the sum and dot product of two matrices. 
-#Next, write a program that imports this library module and use it to perform calculations.
-# You CAN'T use numpy.
-#Examples:
-#
-# matrix_1 = Matrix(4.,5.,6.,7.)
-# matrix_2 = Matrix(2.,2.,2.,1.)
-#
-# matrix_3 = matrix_2 @ matrix_1
-# matrix_4 = matrix_2 + matrix_1
-# matrix_4 = 6 + matrix_1
-# matrix_4 = matrix_1 + 6
-#
-# expand your solution to include other operations like
-# - subtraction 
-# - inversion
-# - string representation 
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#Delete these comments before commit!
-#
-#Good luck.
+if __name__ == '__main__':
+    matrix_1 = Matrix2x2(4., 5., 6., 7.)
+    matrix_2 = Matrix2x2(2., 2., 2., 1.)
+
+    print(f'Matrix 1:\n{matrix_1}\n')
+    print(f'Matrix 2:\n{matrix_2}\n')
+    print(f'Sum of matrices:\n{matrix_1 + matrix_2}\n')
+    print(f'Matrix 1 plus 5.3:\n{matrix_1 + 5.3}\n')
+    # print(f'Matrix 1 plus 5.3:\n{5 + matrix_1}\n')
