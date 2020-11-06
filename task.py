565673c435c4742309e89860d3f5e8c78ca0090f
d0mk <dominik.kobyra@gmail.com>
2020-11-06 13:03:46 +0100

    upload fix

--- task.py
+++ task.py
@@ -13,6 +13,9 @@ class Matrix2x2:
         else:
             print('Argument must be of type Matrix2x2')
 
+    def __radd__(self, m2):
+        return self.__add__(m2)
+
     def __str__(self):
         return f'{self.data[0]} {self.data[1]}\n{self.data[2]} {self.data[3]}'
 
@@ -25,4 +28,4 @@ if __name__ == '__main__':
     print(f'Matrix 2:\n{matrix_2}\n')
     print(f'Sum of matrices:\n{matrix_1 + matrix_2}\n')
     print(f'Matrix 1 plus 5.3:\n{matrix_1 + 5.3}\n')
-    # print(f'Matrix 1 plus 5.3:\n{5 + matrix_1}\n')
+    print(f'5.3 + Matrix 1:\n{5.3 + matrix_1}\n')
